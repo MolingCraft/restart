9c76f02f737aace0c3b8f4b730bb0b5b2c74a09a
MolingCraft <114900756+MolingCraft@users.noreply.github.com>
2023-03-28 09:23:16 +0800

    0.541

--- Assets/Scripts/Data/CharaData/CharaData.cs
+++ Assets/Scripts/Data/CharaData/CharaData.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+/*
 public enum Role//职业
 {
     Warrior,//战士
     Mage,//法师
-}
+}*/
 
 [System.Serializable]
 public class CharaData
 {
     //所有的Lock都代表该值是否锁定不变
     public string charaName;//角色名
-    public Role role;//职业
     public string introduction;//介绍
     public int maxActionNum;//最大行动点数
     public float HP;//HP
@@ -21,6 +21,7 @@ public class CharaData
     public float defend;//防御
     public float size;//体型大小
     public float damage;//攻击力
+    public float attackCD;//攻击CD
     public int affixMaxNum;//词缀上限
 
     public List<long> affixList;
--- Assets/Scripts/Manager/Chara/CharaManager.cs
+++ Assets/Scripts/Manager/Chara/CharaManager.cs
@@ -23,12 +23,10 @@ public class CharaManager : Singleton<CharaManager>
     [Header("种族与武器文件")]
 
     public List<CharaData> RaceList=new List<CharaData>();
-    /*
+
     public TextAsset RacecsvFile;//种族csv文件
-    public TextAsset WeaponcsvFile;//武器csv文件
-    public Dictionary<int, RaceData> RaceDict = new Dictionary<int, RaceData>();
-    public Dictionary<int, EquipmentData> weaponDict = new Dictionary<int, EquipmentData>();
-*/
+    public Dictionary<String, CharaData> RaceDict = new Dictionary<String, CharaData>();
+
     [Space(20)]
     [Header("已生成对象")]
     public GameObject charaObjectfather;
@@ -43,39 +41,22 @@ public class CharaManager : Singleton<CharaManager>
     {
         base.Awake();
 
-        /*
         if (RacecsvFile == null) return;
         RaceDict.Clear();
 
-
-
-        if (WeaponcsvFile == null) return;
-        weaponDict.Clear();*/
-/*
         string[] line = RacecsvFile.text.Split(separator: '\n');//以换行符进行拆分，将文本的内容拆成一行一行的存储
 
         string[] attributename = line[1].Split(separator: ',');//以逗号(csv格式以逗号进行数据分隔)进行拆分
-        attributearrayLength = attributename.Length;
+        var attributearrayLength = attributename.Length;
         for (int i = 2; i < line.Length; i++)
         {
             string[] value = line[i].Split(separator: ',');//以逗号(csv格式以逗号进行数据分隔)进行拆分
-            CharaRace charaRace = new CharaRace(value.Length);
+            CharaData chara=new CharaData();
+            //接下来开始具体数据读取
 
-            charaRace.attributeNameArray[0] = value[0];//种族名
-            charaRace.attributeValueArray[0] = i - 1;//种族编号，虽然我也不知道有啥用
-
-            for (int j = 1; j < value.Length; j++)//属性名
-            {
-                charaRace.attributeNameArray[j] = attributename[j];
-            }
-
-            for (int j = 1; j < value.Length; j++)
-            {
-                charaRace.attributeValueArray[j] = float.Parse(value[j]);
-            }
-            charaRaceDict.Add(i - 1, charaRace);
+            RaceDict.Add(chara.charaName, chara);
         }
-*/
+
 
     }
 
